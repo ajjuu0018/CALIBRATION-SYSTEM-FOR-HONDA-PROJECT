9f24414cd39374da57eca0ec5a6c9f1a7a7dfaba
ajjuu0018 <ajaydamayan@gmail.com>
2026-06-11 18:07:37 +0530

    fix db connection

--- models/db.py
+++ models/db.py
@@ -1,11 +1,10 @@
-import os
 import mysql.connector
 
 def get_db():
     return mysql.connector.connect(
-        host=os.getenv("gateway01.ap-southeast-1.prod.aws.tidbcloud.com"),
-        user=os.getenv("2ny8YgVnFjBwAuU.root"),
-        password=os.getenv("2KT8DkzAoEqt8cUj"),
-        database=os.getenv("calibration_system"),
-        port=int(os.getenv("4000", 4000))
+        host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
+        user="2ny8YgVnFjBwAuU.root",
+        password="2KT8DkzAoEqt8cUj",
+        database="calibration_system",
+        port=4000
     )
